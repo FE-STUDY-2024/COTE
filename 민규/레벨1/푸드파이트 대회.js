8315ada581e1c07f9162c1c5c4d4071bde7e129d
Minkyu0424 <yng1404@gmail.com>
2024-04-03 01:43:26 +0900

    level1 : 푸드파이트 대회

---
+++ 민규/레벨1/푸드파이트 대회.js
@@ -0,0 +1,13 @@
+function solution(food) {
+  const arr = [];
+
+  for (i = 1; i < food.length; i++) {
+    const foods = Math.floor(food[i] / 2);
+    arr.push(String(i).repeat(foods));
+  }
+
+  reverseArr = [...arr].reverse();
+  answer = arr.concat(0, reverseArr);
+  
+  return answer.join("");
+}
