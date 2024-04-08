7be18e5b0235cc7a8f18a29b6dd2173dc28dd6f4
hayamaster <dnfxmfkdbzos@naver.com>
2024-04-09 01:31:59 +0900

    chore: prettier 적용

--- 주하/레벨1/콜라 문제.js
+++ 주하/레벨1/콜라 문제.js
@@ -1,11 +1,11 @@
 const solution = (a, b, n) => {
-  let answer = 0;
+  let answer = 0
   while (n / a >= 1) {
-    const divide = Math.floor(n / a);
+    const divide = Math.floor(n / a)
 
-    n += divide * (b - a);
-    answer += b * divide;
+    n += divide * (b - a)
+    answer += b * divide
   }
 
-  return answer;
-};
+  return answer
+}
