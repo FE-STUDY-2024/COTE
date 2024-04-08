e0cdcc71e7567efafe90ea510942438b99f7fe34
hayamaster <dnfxmfkdbzos@naver.com>
2024-04-09 00:58:22 +0900

    level1: [1차] 비밀지도

---
+++ 주하/레벨1/[1차] 비밀지도.js
@@ -0,0 +1,15 @@
+const solution = (n, arr1, arr2) => {
+  const answer = [];
+
+  for (let i = 0; i < n; i++) {
+    answer.push(
+      (arr1[i] | arr2[i])
+        .toString(2)
+        .padStart(n, "0")
+        .replaceAll("1", "#")
+        .replaceAll("0", " ")
+    );
+  }
+
+  return answer;
+};
