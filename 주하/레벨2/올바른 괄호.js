b5a8abecffccf78b0740c5e7269916651c70a268
hayamaster <dnfxmfkdbzos@naver.com>
2024-04-09 01:20:03 +0900

    level2: 올바른 괄호

---
+++ 주하/레벨2/올바른 괄호.js
@@ -0,0 +1,13 @@
+const solution = (s) => {
+  const stack = [];
+
+  s.split("").forEach((char) => {
+    if (stack.at(-1) === "(" && char === ")") {
+      stack.pop();
+    } else {
+      stack.push(char);
+    }
+  });
+
+  return stack.length === 0;
+};
