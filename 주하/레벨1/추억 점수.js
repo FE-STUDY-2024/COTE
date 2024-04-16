d865f18fa84009ea977d0bd5e175804c239aab0d
JUHA KIM <88622675+hayamaster@users.noreply.github.com>
2024-04-16 23:26:17 +0900

    Revert "2주차"

--- 주하/레벨1/추억 점수.js
+++
@@ -1,8 +0,0 @@
-const solution = (name, yearning, photo) =>
-  photo.map((friends) =>
-    friends.reduce(
-      (acc, cur) =>
-        name.indexOf(cur) === -1 ? acc : acc + yearning[name.indexOf(cur)],
-      0
-    )
-  )
