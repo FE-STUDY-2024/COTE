56d7049c9dd2bd46373e48f95799ffc3d308e9a5
은지 <81403265+eunji0714@users.noreply.github.com>
2024-04-29 20:58:19 +0900

    level2: 2016년

---
+++ 은지/레벨1/2016년.js
@@ -0,0 +1,27 @@
+function solution(a, b) {
+  let answer = ''
+  let sum = 0
+  if (a >= 8 && a % 2 === 1) {
+    sum = (a - 1) * 31 - Math.floor((a - 1) / 2) + b
+  } else if (a === 1) {
+    sum = b
+  } else if (a === 2) {
+    sum = b + 31
+  } else {
+    sum = (a - 1) * 31 - Math.floor((a - 1) / 2) + b - 1
+  }
+  answer = (5 + (sum % 7)) % 7
+  if (answer === 1) answer = 'SUN'
+  else if (answer === 2) answer = 'MON'
+  else if (answer === 3) answer = 'TUE'
+  else if (answer === 4) answer = 'WED'
+  else if (answer === 5) answer = 'THU'
+  else if (answer === 6) answer = 'FRI'
+  else answer = 'SAT'
+
+  return answer
+}
+
+let a = 5
+let b = 1
+console.log(solution(a, b))
